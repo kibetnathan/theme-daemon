21780e7811fbdfdafc0fa2afd0ddb401ca8cdf8d
kibetnathan <kibetnat@gmail.com>
2026-07-28 18:16:19 +0300

    chore(pycache): remove pycache from tracking

--- plugins/tmux.py
+++ plugins/tmux.py
@@ -1,5 +1,8 @@
+import os
+import re
+
 from engine.port import ToolPlugin
-from ._base import line_replace, shell
+from ._base import shell
 
 
 class TmuxPlugin(ToolPlugin):
@@ -8,13 +11,22 @@ class TmuxPlugin(ToolPlugin):
         return "tmux"
 
     def apply(self, palette: dict, is_dark: bool) -> bool:
-        return line_replace(
-            "~/.config/tmux/tmux.conf",
-            [('set -g @ukiyo-theme "catppuccin/mocha"',
-              'set -g @ukiyo-theme "catppuccin/mocha"',
-              'set -g @ukiyo-theme "catppuccin/latte"')],
-            is_dark,
+        path = os.path.expanduser("~/.config/tmux/tmux.conf")
+        if not os.path.isfile(path):
+            return False
+        with open(path) as f:
+            content = f.read()
+        target = "mocha" if is_dark else "latte"
+        new, n = re.subn(
+            r'(set -g @ukiyo-theme "catppuccin/)(mocha|latte)(")',
+            rf'\g<1>{target}\g<3>',
+            content,
         )
+        if n == 0:
+            return False
+        with open(path, "w") as f:
+            f.write(new)
+        return True
 
     def reload(self) -> None:
         shell("tmux source-file ~/.config/tmux/tmux.conf")
